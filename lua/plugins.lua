9992cdb08fce45cac73731507acb468e8786cd52
Arseny <arseny@protonmail.com>
2024-02-26 23:57:57 +0100

    feat: null-ls -> none-ls

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
-  use { 'jose-elias-alvarez/null-ls.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use { 'nvimtools/none-ls.nvim', requires = 'nvim-lua/plenary.nvim' }
 
   -- common
   use { 'nvim-tree/nvim-tree.lua', requires = 'nvim-tree/nvim-web-devicons' }
